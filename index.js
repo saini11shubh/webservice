839d4ce312898aa69f402bdb6dcdf0e57b5ba40f
saini11shubh <shubhamsaini86963@gmail.com>
2022-10-11 16:38:04 +0530

    commit

--- index.js
+++ index.js
@@ -18,9 +18,11 @@ app.get('/', (req, res) => {
 })
 
 app.post('/save', async (req, res) => {
+  
   console.log(req.body);
   //console.log(req.body.address.country)
-
+  //console.log(req.body.address.city)
+ //console.log(req.body[address])
   if (!validator.isAlpha(req.body.first_name)) {
     return res.status(400).json({ response: 'Invalid first name' });
   }
@@ -33,10 +35,11 @@ app.post('/save', async (req, res) => {
     return res.status(400).json({ response: 'Invalid mobile no' });
   }
   
-  // if (!validator.isAlpha(req.body.address.city)) {
-  //   //throw new Error('Invalid mobile number!');
-  //   return res.status(400).json({ response: 'Enter valid city'});
-  // }
+  if (!validator.isAlpha(req.body.address.city)) {
+    //console.log(req.body.address.city)
+    //throw new Error('Invalid mobile number!');
+    return res.status(400).json({ response: 'Enter valid city'});
+  }
 
   // if (!validator.isAlpha(req.body.address.state)) {
   //   return res.status(400).json({ response: 'Enter valid state'});
@@ -54,7 +57,7 @@ app.post('/save', async (req, res) => {
 
   let datainfo = new UserData(req.body);
   console.log("DATA IS" + datainfo);
-  //await datainfo.save();
+  await datainfo.save();
   res.json(datainfo);
 });
 
